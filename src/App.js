c4fdcde17f62a630fcf2197c34603d16627df45d
Juan Di Carlo <juanignacio99.ji@gmail.com>
2023-09-18 20:09:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect, memo } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Productos from './components/Productos/productos';
+import Contacto from './components/Contactos/contactos';
+import QuienesSomos from './components/QuienesSomos/quienessomos';
+import FormularioLogin from "./components/Formularios/Inputusuario/InputUsuario"
+import FormularioRegistro from "./components/Formularios/formularioRegistro"
 
 function App() {
+  const [cartCount, setCartCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+      <Router>
+      <Navbar cartCount={cartCount} />
+          <button className='btn btn-primary text-right' onClick={() => setCartCount(cartCount + 1)}>Agregar al carrito</button>
+          <Navbar/>
+   
+        <Routes>
+        
+          <Route path="/productos" component={Productos} />
+          <Route path="/contacto" component={Contacto} />
+          <Route path="/quienes-somos" component={QuienesSomos} />
+        </Routes>
+    </Router>
     </div>
   );
 }
+
+
+
+
+  
+
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container">
+      <Link className="navbar-brand" to="/">Mi Sitio</Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Inicio</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/productos">Productos</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contacto">Contacto</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/quienes-somos">Quiénes Somos</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  )
+
+};
+
+export default Navbar;
